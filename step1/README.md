847b6004d13953026c734401ed0964f0c4ca22e1
Mitsuru GIMA <gima0326@yahoo.co.jp>
2022-03-08 14:35:16 +0900

    Add files via upload

---
+++ step1/README.md
@@ -0,0 +1,15 @@
+# Step1: TODO リスト表示
+
+### 〜「Step1」 でやっていること〜
+
+・[ http://localhost:8280/ ] にアクセスして、ハードコーディングされた TODO リストが表示されることを確認する。<br>
+
+### 〜編集対象ファイル〜
+
+[ ~/src/re_frame_todolist ]
+・core.cljs
+・db.cljs
+・views.cljs
+・subs.cljs
+
+![todo](https://github.com/gima326/re-frame-todolist/blob/main/readme_img/step1.png)
---
+++ step1/src/re_frame_todolist/core.cljs
@@ -0,0 +1,29 @@
+(ns re-frame-todolist.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [re-frame-todolist.events :as events]
+   [re-frame-todolist.views :as views]
+   [re-frame-todolist.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+
+    ;; step1
+    ;; (rdom/render [views/main-panel] root-el)
+    (rdom/render [views/todo-list] root-el)
+
+    ))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
