59b7f8abcd9486aa169cf2c8357507d11b5cccac
Mitsuru GIMA <gima0326@yahoo.co.jp>
2022-03-08 17:12:09 +0900

    Add files via upload

---
+++ step3/README.md
@@ -0,0 +1,27 @@
+# Step3: TODO 完了/取消し
+
+### 〜「Step3」 でやっていること〜
+
+・各 TODO 項目に、チェックボックスを追加する。<br>
+・チェックボックスの ON/OFF で、各 TODO 項目の状態（未完了／完了）を表現する。<br>
+
+### 〜変更対象ファイル〜
+
+[ ~/src/re_frame_todolist/ ]<br>
+
+　・db.cljs<br>
+　・event.cljs<br>
+　・views.cljs<br>
+
+[ ~/resource/public/ ]<br>
+
+　・index.html<br>
+　・css/style.css<br>
+
+　※以下のファイル群は数が多いのでアップロードしてません。<br>
+
+　　[ ~/node_modules/* ]<br>
+　　[ ~/resource/public/js/compiled/* ]<br>
+
+![todo](https://github.com/gima326/re-frame-todolist/blob/main/readme_img/step3-1.png)
+![todo](https://github.com/gima326/re-frame-todolist/blob/main/readme_img/step3-2.png)
---
+++ step3/src/re_frame_todolist/events.cljs
@@ -0,0 +1,41 @@
+(ns re-frame-todolist.events
+  (:require
+   [re-frame.core :as re-frame]
+   [re-frame-todolist.db :as db]
+   ))
+
+(re-frame/reg-event-db
+ ::initialize-db
+ (fn [_ _]
+   db/default-db))
+
+;; step2
+
+(defn- allocate-next-id
+  [todos]
+  (-> todos
+      keys
+      last
+      ((fnil inc 0))))
+
+(re-frame/reg-event-db
+ ::add-todo
+ (fn [db [event title]]
+   (let [id (allocate-next-id (:todos db))
+
+         ;; step3 項目「completed」追加
+         new-todo {:id id :title title :completed false}]
+
+     ;; step3
+     ;;(update db :todos #(assoc % id {:id id :title title}))
+     (update db :todos #(assoc % id new-todo))
+     )))
+
+;; step3
+
+(re-frame/reg-event-db
+
+
+ ::toggle
+ (fn [db [event id]]
+   (update-in db [:todos id :completed] not)))
